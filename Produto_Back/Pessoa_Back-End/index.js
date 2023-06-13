cf152e880860fdad6ad655aefbdbbb9f9d6af3f8
euanaflor3s <anacarlafloressilva@gmail.com>
2023-06-13 13:59:37 -0400

    alterando as variaveis

--- Produto_Back/Pessoa_Back-End/index.js
+++ Produto_Back/Pessoa_Back-End/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const Pessoa = require("./models/pessoa");
+const Produto = require("./models/produto");
 const cors= require('cors');
 const app = express();
 app.use(cors());
@@ -11,46 +11,71 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/pessoas', async function(req, res){
+app.get('/produtos', async function(req, res){
   try {
-    var pessoas = await Pessoa.select();
-    res.json(pessoas.rows);
+    var produtos = await Produto.select();
+    res.json(produtos.rows);
   } catch (error) {
-    console.error('Erro ao buscar pessoas:', error);
-    res.status(500).json({ error: 'Ocorreu um erro ao buscar pessoas' });
+    console.error('Erro ao buscar produtos:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao buscar produtos' });
   }
 });
 
-app.post('/pessoas', async function(req, res){
+
+
+app.post('/produtos', async function(req, res){
   try {
-    var pessoa = await Pessoa.selectOne(req.body.id);
-    res.json(pessoa.rows[0]);
+    var produto = await Produto.selectOne(req.body.id);
+    res.json(produto.rows[0]);
   } catch (error) {
-    console.error('Erro ao buscar pessoas:', error);
-    res.status(500).json({ error: 'Ocorreu um erro ao buscar pessoas' });
+    console.error('Erro ao buscar produtos:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao buscar produtos' });
   }
 });
 
-app.post('/pessoa', async function(req,res){
+//Essa rota é do tipo post e funciona para inserir um produto
+app.post('/produto', async function(req,res){
   try{
-    var pessoa = req.body
-    var pessoa = await Pessoa.insert(pessoa);
-    res.json(pessoa.rows)
+    var produto = req.body
+    var produto = await Produto.insert(produto);
+    res.json(produto.rows)
   }catch(error){
     console.log("error")
   }
 })
-
-app.delete('/pessoas', async function(req, res){
+//Essa rota é do tipo post e funciona para remover um produto
+app.delete('/produtos', async function(req, res){
   try {
     console.log(req.body.id)
-    var pessoa = await Pessoa.delete(req.body.id);
-    res.json(pessoa.rows);
+    var produto = await Produto.delete(req.body.id);
+    res.json(produto.rows);
   } catch (error) {
-    console.error('Erro ao atualizar pessoa:', error);
-    res.status(500).json({ error: 'Ocorreu um erro ao atualizar pessoa' });
+    console.error('Erro ao atualizar produto:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao atualizar produto' });
   }
 });
+
+//recupera todos os produtos
+app.get('/produto', async function(req, res){
+  try {
+    var produtos = await Produto.select();
+    res.json(produtos.rows);
+  } catch (error) {
+    console.error('Erro ao buscar produtos:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao atualizar produtos' });
+  }
+})
+//altera os dados
+app.put('/produto/:id', async function(req, res){
+  try{
+    var produto = await Produto.update(req.params.id, req.body);
+    res.json(produto.rows);
+  }
+  catch(error){
+    console.log( 'Erro ao atualizar um produto', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao atualizar produto'})
+  }
+})
 
 
 app.listen(3003, function() {
